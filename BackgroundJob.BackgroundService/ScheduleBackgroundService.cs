4c21bb5332aed5d409fc5dc64771903fa1eccd8f
Sammo <sammo.wong@outlook.com>
2020-05-08 17:59:21 +0800

    init

---
+++ BackgroundJob.BackgroundService/ScheduleBackgroundService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundJob.BackgroundService
+{
+    public class ScheduleBackgroundService : Microsoft.Extensions.Hosting.BackgroundService
+    {
+        private readonly ILogger _logger;
+        public ScheduleBackgroundService(ILogger<ScheduleBackgroundService> logger)
+        {
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    //Logic
+                    await Task.Delay(1000 * 1);
+                    Console.WriteLine("running at: " + DateTime.Now.ToString());
+                }
+                catch (Exception ex)
+                {
+                    if (!stoppingToken.IsCancellationRequested)
+                    {
+                        _logger.LogError("ScheduleBackgroundService异常:" + ex.Message + ex.StackTrace);
+                    }
+                    else
+                    {
+                        _logger.LogError("ScheduleBackgroundService停止:" + ex.Message + ex.StackTrace);
+                    }
+                }
+                await Task.Delay(1000 * 60, stoppingToken);//默认等待1分钟
+            }
+        }
+    }
+}
---
+++ BackgroundJob.Timer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Timers;
+
+namespace BackgroundJob.Timer
+{
+    class Program
+    {
+        private static object locker = new object();
+        static void Main(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer()
+            {
+                Enabled = true,
+                Interval = 1000 * 5,//执行间隔时间,单位为毫秒;
+            };
+            timer.Elapsed += new ElapsedEventHandler(Run);
+            timer.Start();
+
+            Console.ReadKey();
+        }
+
+        private static void Run(object source, ElapsedEventArgs e)
+        {
+            lock (locker)
+            {
+                Console.WriteLine("running at: " + DateTime.Now.ToString());
+            }
+        }
+    }
+}
